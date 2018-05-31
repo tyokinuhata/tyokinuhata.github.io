dd5cbeae657a7f772709864e0c935b48ba8764d5
Kazuki Tobita <kazukiti201@gmail.com>
2018-05-31 11:06:58 +0900

    ファビコンのパスを修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ module.exports = {
       { hid: 'description', name: 'description', content: 'My portfolio' }
     ],
     link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        { rel: 'icon', type: 'image/x-icon', href: '/portfolio/favicon.ico' }
     ]
   },
   /*
