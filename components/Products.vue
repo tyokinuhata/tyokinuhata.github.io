96d313ecf9cfd3d9d1be25207a0d5b60c89394f9
Kazuki Tobita <kazukiti201@gmail.com>
2019-03-24 14:46:09 +0900

    Modify products

--- components/Products.vue
+++ components/Products.vue
@@ -1,41 +1,87 @@
 <template>
   <ul class="links mt-2">
+
     <li class="link">
       <details>
-        <summary><a class="anchor" href="https://real-juice.herokuapp.com/" target="_blank">Real Juice</a></summary><span>夏を楽しむためのサービスです. Hack U 2017 Osakaに出展しました.</span></details>
+        <summary>
+          <a class="anchor" href="https://real-juice.herokuapp.com/" target="_blank">Real Juice</a>
+        </summary>
+        <span>夏を楽しむためのサービスです. Hack U 2017 Osakaに出展しました.</span>
+      </details>
     </li>
+
     <li class="link">
       <details>
-        <summary><a class="anchor" href="https://b00kshe1f.herokuapp.com/" target="_blank">Bookshelf</a></summary><span>書籍を管理するためのサービスです.</span></details>
+        <summary>
+          <a class="anchor" href="https://b00kshe1f.herokuapp.com/" target="_blank">Bookshelf</a>
+        </summary>
+        <span>書籍を管理するためのサービスです.</span>
+      </details>
     </li>
+
     <li class="link">
       <details>
-        <summary><a class="anchor" href="https://chat-mammoth.herokuapp.com/" target="_blank">chat mammoth</a></summary><span>手書きでチャットができるサービスです.</span></details>
+        <summary>
+          <a class="anchor" href="https://chat-mammoth.herokuapp.com/" target="_blank">chat mammoth</a>
+        </summary>
+        <span>手書きでチャットができるサービスです.</span>
+      </details>
     </li>
+
     <li class="link">
       <details>
-        <summary><a class="anchor" href="https://tyokinuhata.github.io/zakkuri-bra/" target="_blank">ざっくりBRA</a></summary><span>おっぱいのサイズに応じた最適なカップ数を算出するツールです.</span></details>
+        <summary>
+          <a class="anchor" href="https://tyokinuhata.github.io/zakkuri-bra/" target="_blank">ざっくりBRA</a>
+        </summary>
+        <span>おっぱいのサイズに応じた最適なカップ数を算出するツールです.</span>
+      </details>
     </li>
+
     <li class="link">
       <details>
-        <summary><a class="anchor" href="https://tyokinuhata.github.io/trouble-traveler/" target="_blank">Trouble Traveler</a></summary><span>旅行先ルーレット🗾🗻🗼⛩</span></details>
+        <summary>
+          <a class="anchor" href="https://tyokinuhata.github.io/trouble-traveler/" target="_blank">Trouble Traveler</a>
+        </summary>
+        <span>旅行先ルーレット🗾🗻🗼⛩</span>
+      </details>
     </li>
+
     <li class="link">
       <details>
-        <summary><a class="anchor" href="https://www.npmjs.com/package/codic-cli" target="_blank">codic-cli</a></summary><span>変数やメソッド名の適切な名前を提案をしてくれるCLIアプリケーションです.</span></details>
+        <summary>
+          <a class="anchor" href="https://www.npmjs.com/package/codic-cli" target="_blank">codic-cli</a>
+        </summary>
+        <span>変数やメソッド名の適切な名前を提案をしてくれるCLIアプリケーションです.</span>
+      </details>
     </li>
+
     <li class="link">
       <details>
-        <summary><a class="anchor" href="https://packagist.org/packages/tyokinuhata/html-builder" target="_blank">HTML Builder</a></summary><span>PHPのメソッドチェーンでHTMLとCSSを記述できるライブラリです.</span></details>
+        <summary>
+          <a class="anchor" href="https://packagist.org/packages/tyokinuhata/html-builder" target="_blank">HTML Builder</a>
+        </summary>
+        <span>PHPのメソッドチェーンでHTMLとCSSを記述できるライブラリです.</span>
+      </details>
     </li>
+
     <li class="link">
       <details>
-        <summary><a class="anchor" href="https://github.com/tyokinuhata/gyakupo" target="_blank">逆ポ</a></summary><span>電卓.</span></details>
+        <summary>
+          <a class="anchor" href="https://github.com/tyokinuhata/gyakupo" target="_blank">逆ポ</a>
+        </summary>
+        <span>電卓.</span>
+      </details>
     </li>
+
     <li class="link">
       <details>
-        <summary><a class="anchor" href="https://todoom.herokuapp.com" target="_blank">todoom</a></summary><span>面白いTODOアプリです.</span></details>
+        <summary>
+          <a class="anchor" href="https://todoom.herokuapp.com" target="_blank">todoom</a>
+        </summary>
+        <span>面白いTODOアプリです.</span>
+      </details>
     </li>
+
   </ul>
 </template>
 
