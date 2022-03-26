9c9906857d37c476f5f93acda4aca29302f79a53
Kazuki Tobita <kazukiti201@gmail.com>
2022-03-27 06:25:53 +0900

    Add portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import portfolio from './portfolio.json';
+import icon from './icon.png';
+import { About, Certification, Certifications, JobHistories, JobHistory, Link, Links, StudyHistories, StudyHistory } from './portfolio';
 
 function App() {
+  const about: About = portfolio.about
+  const links: Links = portfolio.links
+  const certifications: Certifications = portfolio.certifications
+  const studyHistories: StudyHistories = portfolio.study_histories
+  const jobHistories: JobHistories = portfolio.job_histories
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img src={icon} className="App-logo" alt="logo" width="200" />
+      <h1>About</h1>
+      <ul>
+        <li>Name: {about.name.hn}({about.name.rn.first} {about.name.rn.last})</li>
+        <li>Email: <a href={`mailto:${about.email}`}>{about.email}</a></li>
+        <li>Location: {about.location}</li>
+        <li>Birthday: {about.birthday}</li>
+      </ul>
+      <h1>Links</h1>
+      <ul>
+        {links.map((link: Link) => {
+          return (
+            <li>
+              <a href={link.url} target="_blank" rel='noreferrer'>{link.name}</a>
+            </li>
+          )
+        })}
+      </ul>
+      <h1>Certifications</h1>
+      <ul>
+        <li>
+          {certifications.map((certification: Certification) => {
+            return (
+              <li>{certification.name}({certification.year})</li>
+            )
+          })}
+        </li>
+      </ul>
+      <h1>Study Histories</h1>
+      <ul>
+        <li>
+          {studyHistories.map((history: StudyHistory) => {
+            return (
+              <li>{history.name} {history.dept}({history.begin}~{history.end})</li>
+            )
+          })}
+        </li>
+      </ul>
+      <h1>Job Histories</h1>
+      <ul>
+        <li>
+          {jobHistories.map((history: JobHistory) => {
+            return (
+              <li>{history.name}({history.begin}~{history.end})</li>
+            )
+          })}
+        </li>
+      </ul>
     </div>
   );
 }
---
+++ src/portfolio.ts
@@ -0,0 +1,42 @@
+export interface About {
+  name: {
+    rn: {
+      first: string;
+      last: string;
+    };
+    hn: string;
+  },
+  email: string;
+  location: string;
+  birthday: string;
+}
+
+export interface Link {
+  name: string;
+  url: string;
+}
+
+export type Links = Array<Link>
+
+export interface Certification {
+  name: string;
+  year: number;
+}
+
+export type Certifications = Array<Certification>
+
+interface History {
+  name: string;
+  begin: number;
+  end?: number;
+}
+
+export interface StudyHistory extends History {
+  dept: string;
+}
+
+export type StudyHistories = Array<StudyHistory>
+
+export type JobHistory = History
+
+export type JobHistories = Array<JobHistory>
